9492adfa6e58a7f880e97a5aac4edcad2c82c6f2
dsannicolasfernandez <64709096+dsannicolasfernandez@users.noreply.github.com>
2021-06-14 13:36:16 +0200

    Create pwmled.py

---
+++ pwmled.py
@@ -0,0 +1,29 @@
+// Programa para modificar la intensidad de un LED
+int LED = 3;                // Salida PWM a utilizar con el LED.
+void setup() {
+  // Bloque de código de configuración.
+  pinMode(LED, OUTPUT);
+}
+void loop() {
+  analogWrite(LED, 50);     // Enciende el LED
+  delay(1000);              // Espera un segundo
+  analogWrite(LED, 100);    // Enciende el LED
+  delay(1000);
+  analogWrite(LED, 150);    // Enciende el LED
+  delay(1000);
+  analogWrite(LED, 200);    // Enciende el LED
+  delay(1000);
+  analogWrite(LED, 250);    // Enciende el LED
+  delay(1000);
+  
+  analogWrite(LED, 200);    // Enciende el LED
+  delay(1000);              // Espera un segundo
+  analogWrite(LED, 150;     // Enciende el LED
+  delay(1000);
+  analogWrite(LED, 100;     // Enciende el LED
+  delay(1000);
+  analogWrite(LED, 5);      // Enciende el LED
+  delay(1000);
+  analogWrite(LED, 0);      // Enciende el LED
+  delay(1000);
+}
